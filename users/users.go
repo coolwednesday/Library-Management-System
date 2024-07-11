56236232dd5c4ef84fdea8be5c271013320a0ee5
Raramuri <raramuri@172-0-189-101.lightspeed.tukrga.sbcglobal.net>
2024-07-11 18:24:34 +0530

    Changes

--- users/users.go
+++ users/users.go
@@ -2,25 +2,28 @@ package users
 
 import "fmt"
 
+// User struct
 type User struct {
 	name string
 	id   int
 }
 
-func AddUsers(existing []User, name string, id int) ([]User, error) {
+// AddUser method add the User into existing list of users and return the modifies list and error if any
+func (u User) AddUsers(existing []User) ([]User, error) {
 	for _, user := range existing {
-		if user.id == id {
-			if user.name == name {
+		if user.id == u.id {
+			if user.name == u.name {
 				return existing, fmt.Errorf("User already exists")
 			}
-			return existing, fmt.Errorf("User with %v id already exists.Try Again", id)
+			return existing, fmt.Errorf("User with %v id already exists.Try Again", u.id)
 		}
 	}
-	existing = append(existing, User{name, id})
+	existing = append(existing, u)
 	return existing, nil
 }
 
-func RemoveUser(existing []User, id int) ([]User, error) {
+// RemoveUser method removes the user by their user id and then returns modified list of users and errors if any
+func (u User) RemoveUser(existing []User, id int) ([]User, error) {
 
 	if len(existing) == 0 {
 		return existing, fmt.Errorf("No Users are present")
@@ -35,7 +38,8 @@ func RemoveUser(existing []User, id int) ([]User, error) {
 
 }
 
-func ListUser(existing []User, id int) (User, error) {
+// ListUser method returns the user that has the same id as argument as well as errors if any
+func (u User) ListUser(existing []User, id int) (User, error) {
 
 	if len(existing) == 0 {
 		return User{}, fmt.Errorf("No Users are present")
